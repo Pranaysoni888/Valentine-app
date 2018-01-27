f936d53787fd5f252b3f7f4947925591b46ea67a
Pranay Soni <pranaysoni888@gmail.com>
2018-01-28 01:07:15 +0530

    Designed Main Activity (Festival Screen).

---
+++ app/src/main/java/com/codepuran/grabby/CustomTextView.java
@@ -0,0 +1,56 @@
+package com.codepuran.grabby;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by parth on 1/27/2018.
+ */
+
+
+
+
+public class CustomTextView extends TextView {
+
+    // Can also be replaced with EditText or Button
+
+    private static final String TAG = "CustomTextView";
+
+    public CustomTextView(Context context) {
+        super(context);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setCustomFont(context, attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setCustomFont(context, attrs);
+    }
+
+    private void setCustomFont(Context ctx, AttributeSet attrs) {
+        TypedArray a = ctx.obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+        String customFont = a.getString(R.styleable.CustomTextView_customFont);
+        setCustomFont(ctx, customFont);
+        a.recycle();
+    }
+
+    public boolean setCustomFont(Context ctx, String asset) {
+        Typeface typeface = null;
+        try {
+            typeface = Typeface.createFromAsset(ctx.getAssets(),"fonts/"+asset);
+        } catch (Exception e) {
+            Log.e(TAG, "Unable to load typeface: "+e.getMessage());
+            return false;
+        }
+
+        setTypeface(typeface);
+        return true;
+    }
+}
--- app/src/main/java/com/codepuran/grabby/activities/MainActivity.java
+++ app/src/main/java/com/codepuran/grabby/activities/MainActivity.java
@@ -1,15 +1,35 @@
 package com.codepuran.grabby.activities;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.codepuran.grabby.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+    private TextView txtValentine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        txtValentine = (TextView) findViewById(R.id.txt_valentine);
+
+        setSupportActionBar(toolbar);
+        if(toolbar!=null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            TextView textView = (TextView) toolbar.findViewById(R.id.txt_toolbar_header);
+            textView.setText(getString(R.string.app_name));
+            Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/bulletto_killa_regular.ttf");
+            textView.setTypeface(typeface);
+        }
+
     }
 }
